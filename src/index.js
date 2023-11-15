b863903cc7e0bce0190d7bbd14d093ead2a04da6
Jakub Jankowski <jakubj@stoplight.io>
2023-11-15 10:49:00 -0600

    mosaic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const global = global || window;
+process = global.process;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
